f55c11af14db5190a048714e29d309d91f3e347b
shahmeen-shaikh <shahmeen_shaikh@outlook.com>
2022-08-18 15:20:40 +0100

    added sql files

---
+++ AnimalKingdom-HWA/src/main/resources/testchema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `animal`;
+
+CREATE TABLE `animal` (
+	`id` BIGINT AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`genus` VARCHAR(255) NOT NULL,
+    `dietary_group` VARCHAR(255) NOT NULL,
+	`age` INT NOT NULL,
+	PRIMARY KEY(`id`)
+);
